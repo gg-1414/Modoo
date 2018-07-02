2d5f8ea901b48b2171dda9bdba8fb20246993dfc
Gina Lee <ginalee@Ginas-MacBook-Pro.local>
2018-07-01 19:11:50 -0700

    Add existing file

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 window.onload = function(){
 
-  var aboutModal = document.getElementById('about-modal');
-  var contactModal = document.getElementById('contact-modal');
+  // modal
+  var aboutModal = document.getElementById("about-modal");
+  var contactModal = document.getElementById("contact-modal");
+  var dineInModal = document.getElementById("dine-in-modal");
+
   var aboutBtn = document.getElementById("about-btn");
   var contactBtn = document.getElementById("contact-btn");
-  var close = document.getElementsByClassName("close")[0];
+  var dineInBtn = document.getElementById("dine-in-btn");
+
+  var closeAbout = document.getElementsByClassName("close")[0];
+  var closeContact = document.getElementsByClassName("close")[1];
+  var closeDineIn = document.getElementsByClassName("close")[2];
 
   aboutBtn.onclick = function() {
       aboutModal.style.display = "block";
@@ -14,15 +21,27 @@ window.onload = function(){
       contactModal.style.display = "block";
   }
 
-  close.onclick = function() {
+  dineInBtn.onclick = function() {
+      dineInModal.style.display = "block";
+  }
+
+  closeAbout.onclick = function() {
       aboutModal.style.display = "none";
+  }
+
+  closeContact.onclick = function() {
       contactModal.style.display = "none";
   }
 
+  closeDineIn.onclick = function() {
+      dineInModal.style.display = "none";
+  }
+
   window.onclick = function(event) {
-      if (event.target == aboutModal || event.target == contactModal) {
+      if (event.target == aboutModal || event.target == contactModal || event.target == dineInModal) {
           aboutModal.style.display = "none";
           contactModal.style.display = "none";
+          dineInModal.style.display = "none";
       }
   }
 
